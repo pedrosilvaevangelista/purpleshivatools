714c0644fd5da4710232a85821a8730ba029e2f3
= <=>
2025-02-15 02:05:09 -0300

    v0.1 initial commit

---
+++ src/alpha/main.py
@@ -0,0 +1,21 @@
+BOLD = "\033[1m"
+PURPLE = "\033[38;2;130;62;176m"
+RESET = "\033[0m"
+TITLE = r"""
+ /#######                                /##                  /######  /##       /##                           /########                  /##          
+| ##__  ##                              | ##                 /##__  ##| ##      |__/                          |__  ##__/                 | ##          
+| ##  \ ## /##   /##  /######   /###### | ##  /######       | ##  \__/| #######  /## /##    /## /######          | ##  /######   /###### | ##  /#######
+| #######/| ##  | ## /##__  ## /##__  ##| ## /##__  ##      |  ###### | ##__  ##| ##|  ##  /##/|____  ##         | ## /##__  ## /##__  ##| ## /##_____/
+| ##____/ | ##  | ##| ##  \__/| ##  \ ##| ##| ########       \____  ##| ##  \ ##| ## \  ##/##/  /#######         | ##| ##  \ ##| ##  \ ##| ##|  ###### 
+| ##      | ##  | ##| ##      | ##  | ##| ##| ##_____/       /##  \ ##| ##  | ##| ##  \  ###/  /##__  ##         | ##| ##  | ##| ##  | ##| ## \____  ##
+| ##      |  ######/| ##      | #######/| ##|  #######      |  ######/| ##  | ##| ##   \  #/  |  #######         | ##|  ######/|  ######/| ## /#######/
+|__/       \______/ |__/      | ##____/ |__/ \_______/       \______/ |__/  |__/|__/    \_/    \_______/         |__/ \______/  \______/ |__/|_______/ 
+                              | ##                                                                                                                     
+                              | ##                                                                                                                     
+                              |__/                                                                                                                     
+"""
+print(f"{BOLD}-=-" * 51 + f"\n\n{BOLD}{PURPLE}{TITLE}{RESET}\n\n" + f"{BOLD}-=-" * 51 + f"\n\n{BOLD}© 2025 - Developed by: {PURPLE}Purple Shiva Team 🔱{RESET}\n" + f"\n{BOLD}{PURPLE}More info at: <https://github.com/PurpleShivaTeam/purpleshivatools>{RESET}\n" + f"""
+{BOLD}This program is free software: you can redistribute it and/or modify it under the terms 
+of the GNU General Public License as published by the Free Software Foundation. You should have 
+received a copy of the GNU General Public License along with this program. If not, see 
+<http://www.gnu.org/licenses/>.{RESET}\n\n""")
