7914bee98268040bcec4619df373ccb5bc5b9e75
= <=>
2025-02-21 01:12:56 -0300

    fixing banner size

--- src/alpha/main.py
+++ src/alpha/main.py
@@ -2,39 +2,20 @@ BOLD = "\033[1m"
 PURPLE = "\033[38;2;130;62;176m"
 RESET = "\033[0m"
 TITLE = r"""
- .--..--..--..--..--..--..--..--..--..--..--..--..--..--..--. 
-/ .. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \
-\ \/\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ \/ /
- \/ /`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'\/ / 
- / /\                                                    / /\ 
-/ /\ \                                                  / /\ \
-\ \/ /                                                  \ \/ /
- \/ /        ____  _   _ ____  ____  _     _____         \/ / 
- / /\       |  _ \| | | |  _ \|  _ \| |   | ____|        / /\ 
-/ /\ \      | |_) | | | | |_) | |_) | |   |  _|         / /\ \
-\ \/ /      |  __/| |_| |  _ <|  __/| |___| |___        \ \/ /
- \/ /       |_|   \_____|_| \_\_|   |_____|_____|        \/ /
- {  }            ____  _   _  _ __     ___               {  }     
- / /\           / ___|| | | || |\ \   / / \              / /\ 
-/ /\ \          \___ \| |_| || | \ \ / / _ \            / /\ \
-\ \/ /           ___) |  _  || |  \ V / ___ \           \ \/ /
- \/ /           |____/|_| |_||_|   \_/_/   \_\           \/ /
- {  }             _________   ___  _    ____             {  }
- / /\            |_   _/ _ \ / _ \| |  / ___|            / /\ 
-/ /\ \             | || | | | | | | |  \___ \           / /\ \
-\ \/ /             | || |_| | |_| | |__ __) |           \ \/ /
- \/ /              |_| \___/ \___/|____|____/            \/ / 
- / /\                                                    / /\ 
-/ /\ \                                                  / /\ \
-\ \/ /                                                  \ \/ /
- \/ /                                                    \/ / 
- / /\.--..--..--..--..--..--..--..--..--..--..--..--..--./ /\ 
-/ /\ \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \/\ \
-\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `' /
- `--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'`--'                                                                                                                   
+    ____  _   _ ____  ____  _     _____       ____  _   _  _ __     ___   
+   |  _ \| | | |  _ \|  _ \| |   | ____|     / ___|| | | || |\ \   / / \      
+   | |_) | | | | |_) | |_) | |   |  _|       \___ \| |_| || | \ \ / / _ \
+   |  __/| |_| |  _ <|  __/| |___| |___       ___) |  _  || |  \ V / ___ \ 
+   |_|   \_____|_| \_\_|   |_____|_____|     |____/|_| |_||_|   \_/_/   \_\ 
+                        _________   ___  _    ____           
+                       |_   _/ _ \ / _ \| |  / ___|           
+                         | || | | | | | | |  \___ \           
+                         | || |_| | |_| | |__ __) |           
+                         |_| \___/ \___/|____|____/           
+                                                                                                             
 """
-print(f"{BOLD}-=-" * 21 + f"\n{BOLD}{PURPLE}{TITLE}{RESET}\n" + f"{BOLD}-=-" * 21 + f"\n\n{BOLD}© 2025 - Developed by: {PURPLE}Purple Shiva Team 🔱{RESET}\n" + f"\n{BOLD}{PURPLE}More info at: <https://github.com/PurpleShivaTeam/purpleshivatools>{RESET}\n" + f"""
-{BOLD}This program is free software: you can redistribute it and/or modify it under the terms 
-of the GNU General Public License as published by the Free Software Foundation. You should have 
-received a copy of the GNU General Public License along with this program. If not, see 
+print(f"{BOLD}-=-" * 25 + f"{BOLD}{PURPLE}{TITLE}{RESET}" + f"{BOLD}-=-" * 25 + f"\n\n{BOLD}© 2025 - Developed by: {PURPLE}Purple Shiva Team 🔱{RESET}\n" + f"{BOLD}{PURPLE}More info at: <https://github.com/PurpleShivaTeam/purpleshivatools>{RESET}\n" + f"""
+{BOLD}This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License.
 <http://www.gnu.org/licenses/>.{RESET}\n\n""")
+
+input("Press any key to continue.")
