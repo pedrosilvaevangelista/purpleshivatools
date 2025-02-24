87f0c26c5e4a8459c8c6d8c35b548a29c3df7d1f
= <=>
2025-02-24 01:14:09 -0300

    fixed banner

--- src/alpha/v0_1/purplest/main.py
+++ src/alpha/v0_1/purplest/main.py
@@ -10,18 +10,18 @@ def main():
     BLUE = "\033[38;2;0;0;255m"
     RESET = "\033[0m"
     TITLE = r"""
-        ____  _   _ ____  ____  _     _____       ____  _   _  _ __     ___   
+     ____  _   _ ____  ____  _     _____       ____  _   _  _ __     ___   
     |  _ \| | | |  _ \|  _ \| |   | ____|     / ___|| | | || |\ \   / / \      
     | |_) | | | | |_) | |_) | |   |  _|       \___ \| |_| || | \ \ / / _ \
     |  __/| |_| |  _ <|  __/| |___| |___       ___) |  _  || |  \ V / ___ \ 
     |_|   \_____|_| \_\_|   |_____|_____|     |____/|_| |_||_|   \_/_/   \_\ 
-                            _________   ___  _    ____           
+                         _________   ___  _    ____           
                         |_   _/ _ \ / _ \| |  / ___|           
-                            | || | | | | | | |  \___ \           
-                            | || |_| | |_| | |__ __) |           
-                            |_| \___/ \___/|____|____/           
-                                                                                                                
-    """
+                          | || | | | | | | |  \___ \           
+                          | || |_| | |_| | |__ __) |           
+                          |_| \___/ \___/|____|____/
+
+"""
     print(f"{BOLD}-=-" * 25 + f"{BOLD}{PURPLE}{TITLE}{RESET}" + f"{BOLD}-=-" * 25 + f"\n\n{BOLD}© 2025 - Developed by: {PURPLE}Purple Shiva Team 🔱{RESET}\n" + f"{BOLD}{PURPLE}More info at: <https://github.com/PurpleShivaTeam/purpleshivatools>{RESET}\n" + f"\n{BOLD}This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License.\n<http://www.gnu.org/licenses/>.{RESET}\n\n")
 
     input("Press any key to continue.")
