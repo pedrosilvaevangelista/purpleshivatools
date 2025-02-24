837a03b070cecd921b6e163f90a3e5e9a576151f
= <=>
2025-02-23 23:35:05 -0300

    adding direct linux command support

--- src/alpha/v0_1/setup.py
+++ src/alpha/v0_1/setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[],  
     entry_points={  
         "console_scripts": [
-            "purplest=main:main", 
+            "purplest=v0_1.main:main", 
             "purplest-morsetool=modules.util_morse:main"  
         ]
     }
