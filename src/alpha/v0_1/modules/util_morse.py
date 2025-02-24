0482e5e88e33112fb5af0c6d69a62dea4bcd6c8a
= <=>
2025-02-24 16:29:23 -0300

    adding more info in the terminal option for morse util

--- src/alpha/v0_1/modules/util_morse.py
+++ src/alpha/v0_1/modules/util_morse.py
@@ -56,12 +56,12 @@ def exportFile(text, type):
 
 # terminal() is used if the user is coming from a linux terminal
 def terminal():
-    parser = argparse.ArgumentParser(description="Morse Code Encryption")
-    parser.add_argument("-s", "--sourcefile", nargs="?", const="", help="Source file that contains the text.\nUsage: purplest-morse -d -s encrypted.txt")
+    parser = argparse.ArgumentParser(description="Morse Code Encryption", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-s", "--sourcefile", nargs="?", const="", help="Source file that contains the text.\n""Usage: purplest-morse -d -s encrypted.txt")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-e", "--encrypt", nargs="?", const="", help="Text to encrypt.\nUsage: purplest-morse -e \"string\"")
-    group.add_argument("-d", "--decrypt", nargs="?", const="", help="Text to decrypt.\nUsage: purplest-morse -d \"string\"")
-    parser.add_argument("-x", "--export", help="Path to export the output. \nUsage: purplest -e \"string\" -x /path/output.txt")
+    group.add_argument("-e", "--encrypt", nargs="?", const="", help="Text to encrypt.\n""Usage: purplest-morse -e \"string\"")
+    group.add_argument("-d", "--decrypt", nargs="?", const="", help="Text to decrypt.\n""Usage: purplest-morse -d \"string\"")
+    parser.add_argument("-x", "--export", help="Path to export the output.\n""Usage: purplest -e \"string\" -x /path/output.txt")
 
     args = parser.parse_args()
 
